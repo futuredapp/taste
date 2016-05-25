b0752ae4bcddf80c204b57c30f15c4a4462b17cc
Radim Vaculik <radim.vaculik@gmail.com>
2016-05-25 14:55:11 +0200

    TBitmap: saveBitmapToFile & calculateInSampleSize refactor

--- core/src/main/java/com/thefuntasty/taste/bitmap/TBitmap.java
+++ core/src/main/java/com/thefuntasty/taste/bitmap/TBitmap.java
@@ -5,8 +5,6 @@ import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
 import android.util.Base64;
 
-import com.thefuntasty.taste.Taste;
-
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -28,14 +26,11 @@ public class TBitmap {
 		return BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
 	}
 
-	public static File saveBitmapToFile(Bitmap bitmap, String filename) {
+	public static void saveBitmapToFile(Bitmap bitmap, File f, Bitmap.CompressFormat format, int quality) {
 		FileOutputStream fos = null;
 		try {
-			String filePath = Taste.context().getCacheDir().getPath() + File.separator + filename;
-			fos = new FileOutputStream(filePath);
-			bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
-
-			return new File(filePath);
+			fos = new FileOutputStream(f);
+			bitmap.compress(format, quality, fos);
 		} catch (FileNotFoundException e) {
 			// nothing to do
 		} finally {
@@ -47,28 +42,10 @@ public class TBitmap {
 				}
 			}
 		}
-		return null;
 	}
 
 	public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
-		// Raw height and width of image
-		final int height = options.outHeight;
-		final int width = options.outWidth;
-		int inSampleSize = 1;
-
-		if (height > reqHeight || width > reqWidth) {
-
-			final int halfHeight = height / 2;
-			final int halfWidth = width / 2;
-
-			// Calculate the largest inSampleSize value that is a power of 2 and keeps both
-			// height and width larger than the requested height and width.
-			while ((halfHeight / inSampleSize) > reqHeight && (halfWidth / inSampleSize) > reqWidth) {
-				inSampleSize *= 2;
-			}
-		}
-
-		return inSampleSize;
+		return Math.min(options.outWidth / reqWidth, options.outHeight / reqHeight);
 	}
 
 	public static Bitmap getScaledBitmapFromPath(String path, int size) {
