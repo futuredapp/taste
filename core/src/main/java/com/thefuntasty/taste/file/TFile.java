52c3ee0a6dbe324d25bd4c46014d21718969bd80
Radim Vaculik <radim.vaculik@gmail.com>
2016-05-26 19:37:36 +0200

    TFile.temp(context)

--- core/src/main/java/com/thefuntasty/taste/file/TFile.java
+++ core/src/main/java/com/thefuntasty/taste/file/TFile.java
@@ -1,5 +1,6 @@
 package com.thefuntasty.taste.file;
 
+import android.content.Context;
 import android.os.Environment;
 
 import java.io.File;
@@ -7,7 +8,7 @@ import java.util.UUID;
 
 public class TFile {
 
-	public static File temp() {
-		return new File(Environment.getExternalStorageDirectory(), UUID.randomUUID().toString());
+	public static File temp(Context context) {
+		return new File(context.getExternalCacheDir(), UUID.randomUUID().toString());
 	}
 }
