d2871b40266c8f0942fddd805f894a84f6edf2f5
Lukas -skywall- Sztefek <lukas.sztefek@thefuntasty.com>
2016-07-10 11:35:25 +0200

    added method getCroppedRectBitmap()

--- core/src/main/java/com/thefuntasty/taste/bitmap/TBitmap.java
+++ core/src/main/java/com/thefuntasty/taste/bitmap/TBitmap.java
@@ -66,4 +66,27 @@ public class TBitmap {
 
 		return retVal;
 	}
+
+	public static Bitmap getCroppedRectBitmap(Bitmap srcBmp) {
+		Bitmap dstBmp;
+		if (srcBmp.getWidth() >= srcBmp.getHeight()) {
+			dstBmp = Bitmap.createBitmap(
+					srcBmp,
+					srcBmp.getWidth() / 2 - srcBmp.getHeight() / 2,
+					0,
+					srcBmp.getHeight(),
+					srcBmp.getHeight()
+			);
+		} else {
+			dstBmp = Bitmap.createBitmap(
+					srcBmp,
+					0,
+					srcBmp.getHeight() / 2 - srcBmp.getWidth() / 2,
+					srcBmp.getWidth(),
+					srcBmp.getWidth()
+			);
+		}
+		srcBmp.recycle();
+		return dstBmp;
+	}
 }
