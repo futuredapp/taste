60d012ba3ada422ebcf5723bd8b6d2a4e5dbc6d6
Radim Vaculik <radim.vaculik@gmail.com>
2017-01-03 09:42:36 +0100

    Remove expi, network, permission and version

--- core/src/main/java/com/thefuntasty/taste/version/TVersion.java
+++
@@ -1,9 +0,0 @@
-package com.thefuntasty.taste.version;
-
-import android.os.Build;
-
-public class TVersion {
-	public static boolean isLollipop() {
-		return Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP;
-	}
-}
