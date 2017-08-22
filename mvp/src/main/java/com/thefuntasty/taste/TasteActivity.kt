f8ae24a74d3171143c8c305ee949068c25ad0619
Lukas -skywall- Sztefek <lukas.sztefek@gmail.com>
2017-08-22 22:27:42 +0200

    add missing @CallSuper annotation

--- mvp/src/main/java/com/thefuntasty/taste/TasteActivity.kt
+++ mvp/src/main/java/com/thefuntasty/taste/TasteActivity.kt
@@ -1,12 +1,14 @@
 package com.thefuntasty.taste
 
 import android.os.Bundle
+import android.support.annotation.CallSuper
 import android.support.annotation.LayoutRes
 import android.support.v7.app.AppCompatActivity
 import butterknife.ButterKnife
 
 abstract class TasteActivity : AppCompatActivity() {
 
+	@CallSuper
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		val activityLayout = activityLayout
